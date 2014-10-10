d16d731189190aa27ba2bf81348ce057568d1817
Mike Schoonover <mike@folsomvillage.com>
2014-10-10 13:42:22 -0600

    Updated project files.
    Signed-off-by: Mike Schoonover <mike@folsomvillage.com>

---
+++ OPT EDM LCD PIC.X/build/default/production/_ext/1472/OPT EDM LCD PIC.o.d
@@ -0,0 +1,5 @@
+build/default/production/_ext/1472/OPT\ EDM\ LCD\ PIC.o:  \
+	C:/Users/Mike/Documents/6\ -\ Controlled\ Documents/PIC\ Projects\ Git\ Repositories/OPT\ EDM\ LCD\ PIC/OPT\ EDM\ LCD\ PIC.asm  \
+	C:/Users/Mike/Documents/6\ -\ Controlled\ Documents/PIC\ Projects\ Git\ Repositories/OPT\ EDM\ LCD\ PIC/P16F648A.INC  \
+	C:/Users/Mike/Documents/6\ -\ Controlled\ Documents/PIC\ Projects\ Git\ Repositories/OPT\ EDM\ LCD\ PIC/standard.mac  \
+
