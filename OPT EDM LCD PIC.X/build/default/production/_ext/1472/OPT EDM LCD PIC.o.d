52efedc257e8dc23161bb6951d17242d611ea179
Mike Schoonover <mike@skoonie.com>
2016-03-12 16:36:42 -0600

    Moved IDE project files from the .X folder to the root folder. The .X folder was created when IDE upgraded to the X version.
    Signed-off-by: Mike Schoonover <mike@skoonie.com>

--- OPT EDM LCD PIC.X/build/default/production/_ext/1472/OPT EDM LCD PIC.o.d
+++
@@ -1,5 +0,0 @@
-build/default/production/_ext/1472/OPT\ EDM\ LCD\ PIC.o:  \
-	C:/Users/Mike/Documents/6\ -\ Controlled\ Documents/PIC\ Projects\ Git\ Repositories/OPT\ EDM\ LCD\ PIC/OPT\ EDM\ LCD\ PIC.asm  \
-	C:/Users/Mike/Documents/6\ -\ Controlled\ Documents/PIC\ Projects\ Git\ Repositories/OPT\ EDM\ LCD\ PIC/P16F648A.INC  \
-	C:/Users/Mike/Documents/6\ -\ Controlled\ Documents/PIC\ Projects\ Git\ Repositories/OPT\ EDM\ LCD\ PIC/standard.mac  \
-
